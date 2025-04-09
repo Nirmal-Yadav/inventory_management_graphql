282055b078ea19cc8b6715db2b7378a0d6f43502
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-04-09 20:46:46 +0530

    fixed setup

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { graphqlHTTP } from "express-graphql";
-import { buildSchema } from "graphql";
 import dotenv from "dotenv";
-import { User } from "./models/user.model.js";
-import resolvers from "./utils/resolvers.js";
-import { verifyJwt } from "./middlewares/verifyJwt.middleware.js";
 
 dotenv.config();
 
@@ -24,19 +19,5 @@ app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(cookieParser());
 app.use(express.static("public"));
-
-// GraphQL Endpoint
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: buildSchema(`
-      ${require("./utils/schema.graphql")}`),
-    rootValue: resolvers,
-    graphiql: true,
-  })
-);
-
-// Protect the GraphQL API with JWT
-app.use("/graphql", verifyJwt);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 import { app } from "./app.js";
 import connectDB from "./db/db.js";
 import dotenv from "dotenv";
+import { typeDefs } from "./utils/schema.graphql.js";
+import resolvers from "./utils/resolvers.js";
+import { getUserFromToken } from "./utils/getUserFromToken.js";
+import { ApolloServer } from "apollo-server-express";
 
 dotenv.config({
   path: "./env",
@@ -16,3 +20,28 @@ connectDB()
   .catch(() => {
     console.log("mongo db connection failed");
   });
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    const token = req.headers.authorization?.split(" ")[1] || "";
+    const user = await getUserFromToken(token);
+
+    console.log(user, "app");
+    return { user };
+  },
+});
+
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log(
+      `🚀 Server running at http://localhost:4000${server.graphqlPath}`
+    );
+  });
+}
+
+startServer();
